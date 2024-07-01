a7d6eedbef970e841ce599a520b3481a57edf87e
ABlued <skwodhkswkd@naver.com>
2024-07-01 22:42:42 +0900

    style : 정산 비용 리스트 스타일링

--- src/components/ExpenseMain.jsx
+++ src/components/ExpenseMain.jsx
@@ -1,16 +1,55 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { useRecoilValue } from "recoil";
+import { styled } from "styled-components";
 import { AddExpenseForm } from "./AddExpenseForm";
 import ExpenseTable from "./ExpenseTable";
-
+import { groupNameState } from "../state/groupName";
 function ExpenseMain() {
   return (
-    <div>
-      <h1>ExpenseMain</h1>
-      <AddExpenseForm />
-      <ExpenseTable />
-      <div></div>
-    </div>
+    <Container>
+      <Row>
+        <Col xs={12} sm={5} md={4}>
+          <LeftPane />
+        </Col>
+        <Col>
+          <RightPane />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
 export default ExpenseMain;
+
+const LeftPane = () => (
+  <Container>
+    <AddExpenseForm />
+  </Container>
+);
+
+const RightPane = () => {
+  const groupName = useRecoilValue(groupNameState);
+  return (
+    <StyledContainer>
+      <Row>
+        <StyledGroupName>{groupName || "그룹 이름"}</StyledGroupName>
+      </Row>
+      <Row>
+        <ExpenseTable />
+      </Row>
+    </StyledContainer>
+  );
+};
+
+const StyledContainer = styled(Container)`
+  padding: 100px 31px 100px 31px;
+`;
+
+const StyledGroupName = styled(Container)`
+  margin-bottom: 80px;
+  font-weight: 700;
+  font-size: 48px;
+  line-height: 48px;
+  text-align: center;
+`;
--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -1,33 +1,72 @@
 import React from "react";
 import { Table } from "react-bootstrap";
 import { useRecoilValue } from "recoil";
+import { styled } from "styled-components";
 import { expensesState } from "../state/expense";
+import { OverlayWrapper } from "./shared/OverlayWrapper";
 
 function ExpenseTable() {
   const expense = useRecoilValue(expensesState);
 
   return (
-    <Table data-testid='expenseList' borderless hover responsive>
-      <thead>
-        <tr>
-          <th>날짜</th>
-          <th>내용</th>
-          <th>금액</th>
-          <th>결제자</th>
-        </tr>
-      </thead>
-      <tbody>
-        {expense.map((data, index) => (
-          <tr key={index}>
-            <td>{data.date}</td>
-            <td>{data.desc}</td>
-            <td>{data.amount} 원</td>
-            <td>{data.payer}</td>
+    <OverlayWrapper minHeight={"73vh"}>
+      <Table data-testid='expenseList' borderless hover responsive>
+        <StyledThead>
+          <tr>
+            <th>날짜</th>
+            <th>내용</th>
+            <th>결제자</th>
+            <th>금액</th>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </StyledThead>
+        <StyledBody>
+          {expense.map((data, index) => (
+            <tr key={index}>
+              <td>{data.date}</td>
+              <td>{data.desc}</td>
+              <td>{data.amount} 원</td>
+              <td>{data.payer}</td>
+            </tr>
+          ))}
+        </StyledBody>
+      </Table>
+    </OverlayWrapper>
   );
 }
 
 export default ExpenseTable;
+
+const StyledThead = styled.thead`
+  color: #6b3da6;
+  text-align: center;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 25px;
+
+  th {
+    padding: 15px 8px;
+    min-width: 60px;
+  }
+  @media screen and (max-width: 600px) {
+    font-size: 4vw;
+    line-height: 10px;
+    th {
+      padding: 10px 4px;
+    }
+  }
+`;
+
+const StyledBody = styled.tbody`
+  td {
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 50px;
+    text-align: center;
+
+    @media screen and (max-width: 600px) {
+      font-size: 4vw;
+      line-height: 20px;
+    }
+  }
+`;
